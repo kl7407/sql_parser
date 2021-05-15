83f3fa45e7390eb02994d1e9af618ecc8f654b99
MacWoo <eee7407@naver.com>
2021-05-16 01:10:38 +0900

    Add: error added

--- errors.py
+++ errors.py
@@ -94,6 +94,107 @@ class CharLengthError(Exception):
         return self.value
 
 
+class InsertReferentialIntegrityError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class InsertDuplicatePrimaryKeyError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class InsertTypeMismatchError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class InsertColumnExistenceError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class InsertColumnNonNullableError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class DeleteReferentialIntegrityPassed(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class SelectTableExistenceError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class SelectColumnResolveError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class WhereIncomparableError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class WhereTableNotSpecified(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class WhereColumnNotExist(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class WhereAmbiguousReference(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
 myErrors = [DuplicateColumnDefError, DuplicatePrimaryKeyDefError, ReferenceTypeError, ReferenceNonPrimaryKeyError,
             ReferenceColumnExistenceError, ReferenceTableExistenceError, NonExistingColumnDefError,
-            TableExistenceError, DropReferencedTableError, NoSuchTable, CharLengthError]
+            TableExistenceError, DropReferencedTableError, NoSuchTable, CharLengthError,
+            InsertReferentialIntegrityError, InsertDuplicatePrimaryKeyError, InsertTypeMismatchError,
+            InsertColumnExistenceError, InsertColumnNonNullableError,
+            DeleteReferentialIntegrityPassed, SelectTableExistenceError, SelectColumnResolveError,
+            WhereIncomparableError, WhereTableNotSpecified, WhereColumnNotExist, WhereAmbiguousReference
+            ]
